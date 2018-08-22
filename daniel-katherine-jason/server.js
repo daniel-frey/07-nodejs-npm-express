651b90d72885e9afa77bee0e0b98ddbf564fa563
Daniel Frey <dann.frey@me.com>
2018-08-22 10:32:00 -0700

    server up and running

--- daniel-katherine-jason/server.js
+++ daniel-katherine-jason/server.js
@@ -1,14 +1,25 @@
 'use strict';
 
 const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // REVIEW: POST route needs to parse the body passed in with the request.
 // POST middleware
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send('Not Found');
+});
+
+app.listen(PORT, function() {
+  console.log('Listening on port', PORT);
 });
